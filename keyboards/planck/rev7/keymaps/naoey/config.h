ded0a14d025c03af9f93589513bfff994acbe74c
naoey <naoey@protonmail.ch>
2024-12-02 18:33:40 +0900

    Initial commit

---
+++ keyboards/planck/rev7/keymaps/naoey/config.h
@@ -0,0 +1,27 @@
+/* Copyright 2015-2023 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define MK_VARIANT MK_TYPE_KINETIC
+
+#define MK_KINETIC_MOUSE_MAXS 72
+#define MK_KINETIC_MOUSE_ACCN 22
+#define MK_KINETIC_MOUSE_FRIC 14
+#define MK_KINETIC_MOUSE_DRAG 18
+
+#define MK_KINETIC_WHEEL_MAXS 1
+#define MK_KINETIC_WHEEL_ACCN 16
