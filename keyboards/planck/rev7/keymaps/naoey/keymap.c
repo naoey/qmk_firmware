2bde858e867ec6f5ee40fc0f86def13a6aaca0f1
naoey <naoey@protonmail.ch>
2026-03-23 13:18:56 +0900

    Also swap cmd key

--- keyboards/planck/rev7/keymaps/naoey/keymap.c
+++ keyboards/planck/rev7/keymaps/naoey/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,LT(_NAV, KC_SCLN), KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    _______, KC_LCTL, KC_LALT, KC_LGUI, FUNCTION, MOUSE, SYMBOL, KC_SPC,  _______, _______, _______,   _______
+    _______, KC_LCTL, KC_LALT, FUNCTION, KC_LGUI, MOUSE, SYMBOL, KC_SPC,  _______, _______, _______,   _______
 ),
 
 /* Lower
